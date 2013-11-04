050d446af8023c8d26553ae7573ebe1af54f9c18
J.S. Roy <jroy@chicagobooth.edu>
2013-11-04 09:17:28 -0600

    update SMTP settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,8 +63,17 @@ Gulch2::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'www.gulchsolutions.com' } 
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address => "smtpout.secureserver.net",
+    :port => 25,
+    :user_name => "support@gulchsolutions.com",
+    :password => ENV['SMTP_PASSSWORD'],
+    :authentication => :login
+  }
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
