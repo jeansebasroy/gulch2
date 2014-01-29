9453ccde74501a360316952fd93aeea04a582a22
J.S. Roy <jroy@chicagobooth.edu>
2014-01-29 16:50:26 -0600

    fix phantomjs again 4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Gulch2::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = false
+  # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
@@ -86,6 +86,11 @@ Gulch2::Application.configure do
   # config.autoflush_log = false
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new 
+  config.log_formatter = ::Logger::Formatter.new
+  
+  # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
+  config.force_ssl = false
+ 
 
 end
